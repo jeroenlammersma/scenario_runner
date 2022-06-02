2fceec7b116ceed49746c878ffb77de0db100f80
Jeroen Lammersma <jeroen@lammersma.dev>
2022-06-02 12:45:56 +0200

    FollowLeadVehicleChangingVelocity updated + FollowLeadVehicleTailgating added

--- srunner/scenarios/follow_leading_vehicle_changing_velocity.py
+++ srunner/scenarios/follow_leading_vehicle_changing_velocity.py
@@ -49,7 +49,7 @@ class FollowLeadingVehicleChangingVelocity(BasicScenario):
     self._map = CarlaDataProvider.get_map()
     self._ego_vehicle_transform = config.trigger_points[0]
     self._first_vehicle_speed = 8
-    self._first_vehicle_x_offset = 30
+    self._first_vehicle_x_offset = 40
     self._other_actor_max_brake = 1.0
     self._other_actor_transform = None
     # Timeout of scenario in seconds
@@ -108,34 +108,32 @@ class FollowLeadingVehicleChangingVelocity(BasicScenario):
         "Drive straight while changing velocity")
     _drive_for_distance(self, drive, self._first_vehicle_speed, 50)
 
-    _drive_for_distance(self, drive, 16, 100)
-    _drive_for_distance(self, drive, 15, 10)
-    _drive_for_distance(self, drive, 14, 10)
-    _drive_for_distance(self, drive, 13, 10)
 
-    _drive_for_distance(self, drive, 12, 50)
-    _drive_for_distance(self, drive, 11, 10)
-    _drive_for_distance(self, drive, 10, 10)
-    _drive_for_distance(self, drive, 9, 10)
-    
-    _drive_for_distance(self, drive, 8, 50)
-    
-    _drive_for_distance(self, drive, 16, 50)
+    _drive_for_distance(self, drive, 10, 40)
+    _drive_for_distance(self, drive, 12, 80)
+    _drive_for_distance(self, drive, 11, 20)
+    _drive_for_distance(self, drive, 10, 20)
+
+    _drive_for_distance(self, drive, 12, 10)
+    _drive_for_distance(self, drive, 20, 100)
+    _drive_for_distance(self, drive, 19, 10)
+    _drive_for_distance(self, drive, 18, 10)
+    _drive_for_distance(self, drive, 17, 10)
+    _drive_for_distance(self, drive, 16, 10)
     _drive_for_distance(self, drive, 15, 10)
     _drive_for_distance(self, drive, 14, 10)
     _drive_for_distance(self, drive, 13, 10)
     _drive_for_distance(self, drive, 12, 10)
-    _drive_for_distance(self, drive, 11, 10)
-    _drive_for_distance(self, drive, 10, 10)
+    _drive_for_distance(self, drive, 11, 8)
+    _drive_for_distance(self, drive, 10, 6)
 
-    _drive_for_distance(self, drive, 9, 5)
-    _drive_for_distance(self, drive, 8, 5)
-    _drive_for_distance(self, drive, 7, 4)
-    _drive_for_distance(self, drive, 6, 4)
-    _drive_for_distance(self, drive, 5, 3)
-    _drive_for_distance(self, drive, 4, 2)
-    _drive_for_distance(self, drive, 3, 1)
-    _drive_for_distance(self, drive, 2, 1)
+    _drive_for_distance(self, drive, 16, 50)
+    _drive_for_distance(self, drive, 15, 20)
+    _drive_for_distance(self, drive, 14, 10)
+    _drive_for_distance(self, drive, 13, 8)
+    _drive_for_distance(self, drive, 12, 8)
+    _drive_for_distance(self, drive, 11, 8)
+    _drive_for_distance(self, drive, 10, 8)
 
     # stop vehicle
     stop = StopVehicle(other_actor, self._other_actor_max_brake)
@@ -147,16 +145,15 @@ class FollowLeadingVehicleChangingVelocity(BasicScenario):
                                                     self.ego_vehicles[0],
                                                     distance=20,
                                                     name="FinalDistance")
-    endcondition_part2 = StandStill(
-        self.ego_vehicles[0], name="StandStill", duration=1)
-    endcondition.add_child(endcondition_part1)
-    endcondition.add_child(endcondition_part2)
+    # endcondition_part2 = StandStill(
+    #     self.ego_vehicles[0], name="StandStill", duration=1)
+    # endcondition.add_child(endcondition_part1)
+    # endcondition.add_child(endcondition_part2)
 
     # Build behavior tree
     sequence = py_trees.composites.Sequence("Sequence Behavior")
     sequence.add_child(start_transform)
     sequence.add_child(drive)
-    sequence.add_child(stop)
     sequence.add_child(endcondition)
     sequence.add_child(ActorDestroy(self.other_actors[0]))
 
@@ -341,6 +338,120 @@ class FollowLeadingVehicleChangingVelocityAndStop(BasicScenario):
     sequence.add_child(straight_at_first_intersection)
     sequence.add_child(drive_2)
     sequence.add_child(stop)
+    sequence.add_child(endcondition)
+    sequence.add_child(ActorDestroy(self.other_actors[0]))
+
+    return sequence
+
+  def _create_test_criteria(self):
+    """
+    A list of all test criteria will be created that is later used
+    in parallel behavior tree.
+    """
+    criteria = []
+
+    collision_criterion = CollisionTest(self.ego_vehicles[0])
+
+    criteria.append(collision_criterion)
+
+    return criteria
+
+  def __del__(self):
+    """
+    Remove all actors upon deletion
+    """
+    self.remove_all_actors()
+
+
+class FollowLeadingVehicleTailgating(BasicScenario):
+
+  timeout = 60            # Timeout of scenario in seconds
+
+  def __init__(self, world, ego_vehicles, config, randomize=False, debug_mode=False, criteria_enable=True,
+               timeout=60):
+    """
+    Setup all relevant parameters and create scenario
+
+    If randomize is True, the scenario parameters are randomized
+    """
+
+    self._map = CarlaDataProvider.get_map()
+    self._ego_vehicle_transform = config.trigger_points[0]
+    self._first_vehicle_speed = 27.8
+    self._first_vehicle_x_offset = 50
+    self._other_actor_max_brake = 1.0
+    self._other_actor_transform = None
+    # Timeout of scenario in seconds
+    self.timeout = timeout
+
+    super(FollowLeadingVehicleTailgating, self).__init__("FollowVehicleTailgating",
+                                                               ego_vehicles,
+                                                               config,
+                                                               world,
+                                                               debug_mode,
+                                                               criteria_enable=criteria_enable)
+
+    if randomize:
+      self._ego_other_distance_start = random.randint(4, 8)
+
+  def _initialize_actors(self, config):
+    """
+    Custom initialization
+    """
+
+    self._other_actor_transform = carla.Transform(
+        carla.Location(self._ego_vehicle_transform.location.x + self._first_vehicle_x_offset,
+                       self._ego_vehicle_transform.location.y,
+                       self._ego_vehicle_transform.location.z),
+        self._ego_vehicle_transform.rotation)
+    first_vehicle_transform = carla.Transform(
+        carla.Location(self._other_actor_transform.location.x,
+                       self._other_actor_transform.location.y,
+                       self._other_actor_transform.location.z - 500),
+        self._other_actor_transform.rotation)
+    first_vehicle = CarlaDataProvider.request_new_actor(
+        'vehicle.nissan.patrol', first_vehicle_transform)
+    first_vehicle.set_simulate_physics(enabled=False)
+    self.other_actors.append(first_vehicle)
+
+  def _create_behavior(self):
+    """
+    The scenario defined after is a "follow leading vehicle" scenario. After
+    invoking this scenario, it will wait for the user controlled vehicle to
+    enter the start region, then make the other actor to drive until reaching
+    the next intersection. Finally, the user-controlled vehicle has to be close
+    enough to the other actor to end the scenario.
+    If this does not happen within 60 seconds, a timeout stops the scenario
+    """
+
+    # set other_actor to first other actor
+    other_actor = self.other_actors[0]
+
+    # to avoid the other actor blocking traffic, it was spawed elsewhere
+    # reset its pose to the required one
+    start_transform = ActorTransformSetter(
+        other_actor, self._other_actor_transform)
+
+    # drive straight while changing velocity
+    drive = py_trees.composites.Sequence(
+        "Drive straight while changing velocity")
+    _drive_for_distance(self, drive, self._first_vehicle_speed, 540)
+
+    # stop vehicle
+    stop = StopVehicle(other_actor, self._other_actor_max_brake)
+
+    # end condition
+    endcondition = py_trees.composites.Parallel("Waiting for end position",
+                                                policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ALL)
+    endcondition_part1 = InTriggerDistanceToVehicle(other_actor,
+                                                    self.ego_vehicles[0],
+                                                    distance=20,
+                                                    name="FinalDistance")
+
+    # Build behavior tree
+    sequence = py_trees.composites.Sequence("Sequence Behavior")
+    sequence.add_child(start_transform)
+    sequence.add_child(drive)
     sequence.add_child(endcondition)
     sequence.add_child(ActorDestroy(self.other_actors[0]))
 
